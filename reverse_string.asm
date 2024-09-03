1edce09b2547548f5ff03d6120be78321399eab8
yuriy.tereshchuk <astro.lutsk.aa@gmail.com>
2024-09-03 22:29:30 +0300

    Reverse string.

---
+++ reverse_string.asm
@@ -0,0 +1,28 @@
+format ELF executable 3
+entry _start
+
+segment readable executable
+
+_start:
+  mov ecx, strlen    ; init loop counter
+  mov esi, string    ; ponit source string
+  stackpush:         ; copy source string in stack
+  mov bl, byte [esi]
+  push ebx
+  inc esi
+  loop stackpush     ; end copy in stack
+
+  mov ecx, strlen    ; reinit loop counter
+  mov edi, dst       ; point to destination memory
+
+  stackpop:          ; copy from stack in dest memory
+  pop ebx
+  mov [edi], ebx
+  inc edi
+  loop stackpop      ; end copy from stack
+
+segment readable writable
+  string db "to be reversed"
+  strlen = $ - string
+  dst rb strlen
+
