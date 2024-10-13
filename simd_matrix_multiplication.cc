b340bcbdebad3169e3f7d146d47856692a707c09
yuriy.tereshchuk <astro.lutsk.aa@gmail.com>
2024-10-13 19:36:51 +0300

    SIMD static matrix 16x16 multiplication.

--- simd_matrix_multiplication.cc
+++ simd_matrix_multiplication.cc
@@ -10,16 +10,18 @@
 
 #include <armadillo>
 
-#define _M 10
-#define _K 10
-#define _N 10
+#include <immintrin.h>
+
+#define _M 16
+#define _K 16
+#define _N 16
 
 #define MATRIX_A_SIZE (_M * _K) // cols * rows
 #define MATRIX_B_SIZE (_K * _N) // matrix_b_rows = matrix_a_cols
 #define MATRIX_C_SIZE (_M * _N) // = matrix_a_rows * matrix_b_cols
 
 extern "C" {
-  extern void matrix_mul_asm(const long long int* matrix_a, const long long int* matrix_b, long long int* matrix_c);
+  extern void matrix_mul_asm(const uint16_t* matrix_a, const uint16_t* matrix_b, uint32_t* matrix_c);
 }
 
 
@@ -28,7 +30,7 @@ extern "C" {
  3 4 5 + 3 4 5 = 42 54 66 
  6 7 8   6 7 8   69 90 111
 */
-void matrix_mul(const long long int* matrix_a, const long long int* matrix_b, long long int* matrix_c)
+void matrix_mul(const uint16_t* matrix_a, const uint16_t* matrix_b, uint32_t* matrix_c)
 {
   int prod_id = 0;
   int prod = 0;
@@ -47,7 +49,31 @@ void matrix_mul(const long long int* matrix_a, const long long int* matrix_b, lo
   }
 }
 
-void test(long long int* matrix)
+void simd_matrix_mul(const uint16_t* matrix_a, const uint16_t* matrix_b, uint32_t* matrix_c)
+{
+  uint32_t* tmp = (uint32_t*) aligned_alloc(32, 32);
+  for(int i = 0; i < (_M * 16); i+=16)
+  {
+    const __m256i vec_a = _mm256_load_si256(reinterpret_cast<const __m256i*>(matrix_a + i));
+    for(int j = 0; j < (_N * 16); j+=16)
+    {
+      const __m256i vec_b = _mm256_load_si256(reinterpret_cast<const __m256i*>(matrix_b + j));
+      const __m256i vec_c = _mm256_madd_epi16(vec_a, vec_b);
+      const __m256i S1 = _mm256_hadd_epi32(vec_c, _mm256_setzero_si256());
+      const __m256i S2 = _mm256_hadd_epi32(S1, _mm256_setzero_si256());
+      _mm256_stream_si256((__m256i*) tmp, S2);
+
+      uint32_t prod = *((uint32_t*) tmp);
+      prod += *((uint32_t*) tmp + 4);
+
+      _mm_stream_si32((int*) matrix_c, (int) prod);
+      matrix_c++;
+    }
+  }
+}
+
+template<class T>
+void test(T* matrix)
 {
   int cols = _M;
 
@@ -55,12 +81,12 @@ void test(long long int* matrix)
   {
     if (i < cols)
     {
-      std::cout << matrix[i] << "  ";
+      std::cout << (int) matrix[i] << "  ";
     }
     else
     {
       std::cout << ";" << std::endl;
-      std::cout << matrix[i] << "  ";
+      std::cout << (int) matrix[i] << "  ";
       cols += _M;
     }
   }
@@ -73,22 +99,27 @@ void test(long long int* matrix)
  3 4 5
  6 7 8
 */
-void init(long long int* matrix)
+template<class T>
+void init(T* matrix, int value)
 {
   for (int i = 1; i <= MATRIX_A_SIZE; i++)
   {
-    *matrix++ = i;
+    *matrix++ = value;
   }
 }
 
+typedef arma::Mat<uint16_t> umat16_t;
+typedef arma::Mat<uint32_t> umat32_t;
+typedef arma::Col<uint16_t> uvec16_t;
+
 int main(int argv, char** argc)
 {
-  long long int* matrix_a = new long long int[sizeof(long long int) * MATRIX_A_SIZE];
-  long long int* matrix_b = new long long int[sizeof(long long int) * MATRIX_B_SIZE];
-  long long int* matrix_c = new long long int[sizeof(long long int) * MATRIX_C_SIZE];
+  uint16_t* matrix_a = (uint16_t*) aligned_alloc(32, sizeof(uint16_t) * MATRIX_A_SIZE);
+  uint16_t* matrix_b = (uint16_t*) aligned_alloc(32, sizeof(uint16_t) * MATRIX_B_SIZE);
+  uint32_t* matrix_c = (uint32_t*) aligned_alloc(32, sizeof(uint32_t) * MATRIX_C_SIZE);
 
-  init(matrix_a);
-  init(matrix_b);
+  init(matrix_a, 2);
+  init(matrix_b, 2);
 
   auto begin = std::chrono::steady_clock().now();
   matrix_mul(matrix_a, matrix_b, matrix_c);
@@ -97,15 +128,31 @@ int main(int argv, char** argc)
   std::cout << "Matrix calculation takes: " << duration.count() << "ns." << std::endl;
   test(matrix_c);
 
-  init(matrix_c);
+  init(matrix_c, 0);
   
-  arma::imat A(matrix_a, _M, _K, false, true), B(matrix_b, _K, _N, false, true), C(matrix_c, _M, _N, false, false);
+  umat16_t A(matrix_a, _M, _K, false, true), 
+           B(matrix_b, _K, _N, false, true);
+  umat32_t C(matrix_c, _M, _N, false, false);
   begin = std::chrono::steady_clock().now();
-  C = A * B;
+  C = arma::conv_to<umat32_t>::from(A * B);
   end = std::chrono::steady_clock().now();
   duration = end - begin;
   std::cout << "Armadillo Matrix calculation takes: " << duration.count() << "ns." << std::endl;
   C.print("C:");
+
+  uvec16_t Vb  = arma::vectorise(B);
+  
+  uint16_t* pVb = Vb.memptr();
+
+  init(matrix_c, 0);
+
+  begin = std::chrono::steady_clock().now();
+  simd_matrix_mul(matrix_a, matrix_b, matrix_c);
+  end = std::chrono::steady_clock().now();
+  duration = end - begin;
+  std::cout << "SIMD Matrix calculation takes: " << duration.count() << "ns." << std::endl;
+
+  test(matrix_c);
 
   return EXIT_SUCCESS;
 }
